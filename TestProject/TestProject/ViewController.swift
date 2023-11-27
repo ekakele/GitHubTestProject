b6ceeda404cca40422fdd7c47806f8523087a080
ekakele <ekaakelenjeridze@gmail.com>
2023-11-27 19:22:16 +0400

    add project folder

---
+++ TestProject/TestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Eka Kelenjeridze on 27.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
